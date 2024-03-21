5fed86abcbfed526194729e14f8b65be92a2b1fc
wira07 <wiralodrasaputra07@gmail.com>
2024-03-21 08:55:09 +0700

    update file

---
+++ app/src/main/java/com/example/modul2/Coba_Commit.java
@@ -0,0 +1,14 @@
+package com.example.modul2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Coba_Commit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coba_commit);
+    }
+}
